be0df7b626a3d38fb2dc34073f7fbcf60f9fbc8b
stephenH <sharvick@arrayschool.com>
2017-12-07 16:42:14 -0700

    Fully functioning in 2.5 hours

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable, :omniauthable
+
+  has_many :lists
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :lists do
+    resources :items
+  end
+
+  devise_for :users
+  get 'welcome/index'
+
+  root to: "welcome#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
