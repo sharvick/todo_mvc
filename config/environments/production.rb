cc01088726f0128bb3fdca093028e8544fc80aaa
stephenH <sharvick@arrayschool.com>
2017-12-08 15:13:18 -0700

    Commit for confirmation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'https://calm-stream-18991.herokuapp.com',
+      :domain => 'calm-stream-18991.herokuapp.com',
       :address => 'smtp.sendgrid.net',
       :port => 587,
       :authentication => :plain,
